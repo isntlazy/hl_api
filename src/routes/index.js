5680876d620e41e8de1f1332b61a29dc86d48496
isntlazy <isntlazy@gmail.com>
2019-11-30 20:34:12 +0200

    eslint added for a project

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,10 @@
 // routes/index.js
-import express from 'express';
+import express from 'express'
 
-const router = express.Router();
+const router = express.Router()
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.send('test');
-});
-export default router;
+  res.send('test')
+})
+export default router
